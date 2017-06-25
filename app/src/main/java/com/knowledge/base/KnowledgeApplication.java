83c13a50f4e6c5d01f8fa625ce32951286a2f6a5
张运才 <931267238@qq.com>
2017-06-25 23:00:54 +0800

    移除多余的mipmap包

---
+++ app/src/main/java/com/knowledge/base/KnowledgeApplication.java
@@ -0,0 +1,14 @@
+package com.knowledge.base;
+
+import android.app.Application;
+
+/**
+ * Created by zhangyuncai on 2017/6/25.
+ */
+
+public class KnowledgeApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
